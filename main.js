fc247de2022bfe0f12bfdada1377a363c754363c
Ultrabugs <234222906+Ultrabugs@users.noreply.github.com>
2026-04-19 00:10:15 +0100

    Added more Body and css
    Added CSS and body HTML skeleton now just touching up

---
+++ main.js
@@ -0,0 +1,24 @@
+function add(numberOne, numberTwo) {
+    return numberOne + numberTwo;
+}
+
+function subtract(numberOne, numberTwo) {
+    return numberOne - numberTwo;
+}
+
+function multiply(numberOne, numberTwo) {
+    return numberOne * numberTwo;
+}
+
+function divide(numberOne, numberTwo) {
+    return numberOne / numberTwo;
+}
+
+function operate(operator, numberX, numberY) {
+     
+
+}
+
+
+
+
