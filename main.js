cd66735aff9c1ebe31c84c42c5800c7b2880039c
Ultrabugs <234222906+Ultrabugs@users.noreply.github.com>
2026-04-24 01:04:17 +0100

    Problemed code

--- main.js
+++ main.js
@@ -14,22 +14,44 @@ function divide(numberOne, numberTwo) {
     return numberOne / numberTwo;
 }
 
-function operate(operator, numberX, numberY) {
+function operate(operator, numberX, numberY) { 
+   switch(operator) {
+    case "+":
+        add(numberX, numberY);
+    case "-":
+        subtract(numberX, numberY);
+    case "*":
+        multiply(numberX, numberY);
+    case "/":
+        divide(numberX, numberY)
+   }
 
 }
 
-const firstNumber = document.querySelectorAll("button");
+ 
+const textOutput = document.querySelector(".calculator-results");
 
-firstNumber.array.forEach((button) => {
+const buttons = document.querySelectorAll("button");
+
+let numberOne = null;
+let numberTwo = null;
+let operator = null;
+
+let firstInput = [];
+
+buttons.forEach((button) => {
     button.addEventListener("click", ()=> {
-        console.log(button.id);
+        textOutput.textContent = button.textContent;
+        if(button.textContent != "+" || button.textContent != "-" || button.textContent != "*" || button.textContent != "/" || button.textContent != "x") {
+            console.log(button.textContent);
+            firstInput.push(button.textContent);
+            console.log(firstInput);
+        } else if(button.textContent === "+" || button.textContent === "-" || button.textContent === "*" || button.textContent === "/" || button.textContent === "x") {
+            operator = button.textContent; 
+            console.log(firstInput.toString().slice(",").join())
+        }
+        
     });
 });
-     
-
- 
-
-
-
 
 
