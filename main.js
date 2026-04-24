dc81682fe0927e2f5c49584aa31addbfb0faf4a4
Ultrabugs <234222906+Ultrabugs@users.noreply.github.com>
2026-04-24 07:32:46 +0100

    Stuck on 4 enter a second operator

--- main.js
+++ main.js
@@ -1,29 +1,76 @@
 function add(numberOne, numberTwo) {
-    return numberOne + numberTwo;
+    console.log(totalSum);
+    if(totalSum === null) {
+        let result = Number(numberOne) + Number(numberTwo);
+        textOutput.textContent = result;
+        totalSum = result;
+    } else if(totalSum != null) {
+        numberOne = totalSum;
+        let result = Number(numberOne) + Number(numberTwo);
+        totalSum = result;
+        textOutput.textContent = result;
+    }
+   
 }
 
 function subtract(numberOne, numberTwo) {
-    return numberOne - numberTwo;
+     console.log(totalSum);
+    if(totalSum === null) {
+        let result = Number(numberOne) - Number(numberTwo);
+        textOutput.textContent = result;
+        totalSum = result;
+    } else if(totalSum != null) {
+        numberOne = totalSum;
+        let result = Number(numberOne) - Number(numberTwo);
+        totalSum = result;
+        textOutput.textContent = result;
+    }
 }
 
 function multiply(numberOne, numberTwo) {
-    return numberOne * numberTwo;
+     console.log(totalSum);
+    if(totalSum === null) {
+        let result = Number(numberOne) * Number(numberTwo);
+        textOutput.textContent = result;
+        totalSum = result;
+    } else if(totalSum != null) {
+        numberOne = totalSum;
+        let result = Number(numberOne) * Number(numberTwo);
+        totalSum = result;
+        textOutput.textContent = result;
+    }
 }
 
 function divide(numberOne, numberTwo) {
-    return numberOne / numberTwo;
+     console.log(totalSum);
+    if(totalSum === null) {
+        let result = Number(numberOne) / Number(numberTwo);
+        textOutput.textContent = result;
+        totalSum = result;
+    } else if(totalSum != null) {
+        numberOne = totalSum;
+        let result = Number(numberOne) / Number(numberTwo);
+        totalSum = result;
+        textOutput.textContent = result;
+    }
 }
 
 function operate(operator, numberX, numberY) { 
-   switch(operator) {
-    case "+":
-        add(numberX, numberY);
-    case "-":
-        subtract(numberX, numberY);
-    case "*":
-        multiply(numberX, numberY);
-    case "/":
-        divide(numberX, numberY)
+   console.log(`number one ${numberOne} | number two ${numberTwo}`)
+   if(operator === "+") {
+    add(numberOne, numberTwo)
+   } 
+
+   if(operator === "-") {
+    subtract(numberOne, numberTwo)
+   }
+
+   if(operator === "x") {
+    multiply(numberOne, numberTwo)
+   }
+
+   if(operator === "/") {
+    divide(numberOne, numberTwo)
    }
 
 }
@@ -36,21 +83,51 @@ const buttons = document.querySelectorAll("button");
 let numberOne = null;
 let numberTwo = null;
 let operator = null;
+let totalSum = null;
 
-let firstInput = [];
+let inputArray = [];
 
 buttons.forEach((button) => {
     button.addEventListener("click", ()=> {
+
         textOutput.textContent = button.textContent;
-        if(button.textContent != "+" || button.textContent != "-" || button.textContent != "*" || button.textContent != "/" || button.textContent != "x") {
-            console.log(button.textContent);
-            firstInput.push(button.textContent);
-            console.log(firstInput);
+        
+        if(button.textContent != "+" && button.textContent != "-" && button.textContent != "*" && button.textContent != "/" && button.textContent != "x" && button.textContent != "=" && button.textContent != "Clear") {
+           
+            inputArray.push(button.textContent);
+            console.log(inputArray);
+
         } else if(button.textContent === "+" || button.textContent === "-" || button.textContent === "*" || button.textContent === "/" || button.textContent === "x") {
             operator = button.textContent; 
-            console.log(firstInput.toString().slice(",").join())
+            if(numberOne === null) {
+                numberOne = inputArray.toString().split(",").join("")
+                console.log(`number one ${numberOne}`)
+                inputArray = [];
+            }
+
+            if(numberOne != null & numberTwo != null) {
+                
+                numberTwo = inputArray.toString().split(",").join("")
+                inputArray = [];
+            }
+        } else if (button.textContent === "=") {
+            if(numberOne != null && numberTwo === null || numberTwo != null) {
+               
+                numberTwo = inputArray.toString().split(",").join("")
+                console.log(`number two ${numberTwo}`)
+                operate(operator, numberOne, numberTwo)
+                inputArray = [];
+
+            }
+
+        } else if(button.textContent === "Clear") {
+            numberOne = null;
+            numberTwo = null;
+            totalSum = null;
+            inputArray = [];
+            textOutput.textContent = "";
+             
         }
-        
     });
 });
 
